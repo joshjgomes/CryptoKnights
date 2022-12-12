2858f3399a47465a69e359154d0d48dc2982123e
joshjgomes <joshjgomes@icloud.com>
2022-12-12 14:51:52 -0500

    Second Commit

---
+++ frontend/types/EditionDropMetadata.ts
@@ -0,0 +1,13 @@
+
+
+import { Json } from "@thirdweb-dev/sdk";
+import { BigNumber } from "ethers";
+
+type EditionDropMetadata = {
+  metadata: {
+    [x: string]: Json;
+  };
+  supply: BigNumber;
+};
+
+export default EditionDropMetadata;
